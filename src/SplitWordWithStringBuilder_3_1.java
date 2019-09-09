eb15bea18f565973d0d8f06a8cb68096ce6c396d
ashustov <andrey.maxle@gmail.com>
2019-09-09 13:16:31 +0300

    added tasks: 3_1 3_2

---
+++ src/SplitWordWithStringBuilder_3_1.java
@@ -0,0 +1,10 @@
+public class SplitWordWithStringBuilder_3_1 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("word");
+        for (int i = 0; i < sb.length(); i++) {
+            char ch = sb.charAt(i);
+            System.out.print(ch + "_");
+        }
+    }
+}
