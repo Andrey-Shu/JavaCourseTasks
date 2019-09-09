77f1b3f42946ca861dc566f0c8040634c64647ca
ashustov <andrey.maxle@gmail.com>
2019-09-09 17:07:59 +0300

    added tasks: 4_2

---
+++ src/FindWordsWithCurrentLetter_4_2.java
@@ -0,0 +1,12 @@
+public class FindWordsWithCurrentLetter_4_2 {
+    public static void main(String[] args) {
+        String letter = "q";
+        String str = "qwe qwr trf dfsq q fdsdf  fddd asd";
+        String separator = "\\s+";
+        String[] sepString = str.split(separator);
+        for (int i = 0; i < str.split(separator).length; i++)
+            if (sepString[i].contains(letter)) {
+                System.out.println(sepString[i]);
+            }
+    }
+}
