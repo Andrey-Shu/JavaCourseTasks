d2b85a81cabfc2dd52e582db6d1411fc6e3fa17a
ashustov <andrey.maxle@gmail.com>
2019-09-10 15:30:38 +0300

    updated task: 4_4

--- src/twoDemencialArrayWithMaxAndSum_4_4.java
+++ src/twoDemencialArrayWithMaxAndSum_4_4.java
@@ -1,5 +1,32 @@
+import java.util.Random;
+
 public class twoDemencialArrayWithMaxAndSum_4_4 {
     public static void main(String[] args) {
+        Random random = new Random();
+        int y = 4;
+        int x = 4;
+        int[][] intArray = new int[x][y];
+        int maxValue = 0;
+        int sum = 0;
+        System.out.println("Matrix: ");
+        for (int i = 0; i < x; i++) {
 
+            int num = random.nextInt(100);
+            intArray[0][i] = num;
+            for (int j = 0; j < y; j++) {
+                 num = random.nextInt(100);
+                intArray[i][j] = num;
+                System.out.print(intArray[i][j]+"\t");
+                if (intArray[i][j]>maxValue){
+                    maxValue =intArray[i][j];
+                    sum = sum + intArray[i][j];
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Max value = " + maxValue);
+        System.out.println();
+        System.out.println("Sum = " + sum);
     }
 }
