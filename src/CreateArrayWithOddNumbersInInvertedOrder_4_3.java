3be136d0d7840f2434e140960a4c4ec77c753445
ashustov <andrey.maxle@gmail.com>
2019-09-10 13:27:41 +0300

    updated task: 4_3

--- src/CreateArrayWithOddNumbersInInvertedOrder_4_3.java
+++ src/CreateArrayWithOddNumbersInInvertedOrder_4_3.java
@@ -6,10 +6,12 @@ public class CreateArrayWithOddNumbersInInvertedOrder_4_3 {
             numbers[i] = i + 1;
             if (numbers[i] % 2 == 0) {
                 result[i] = numbers[i];
-  //              System.out.println(result[i]);
+                //              System.out.println(result[i]);
             }
         }
         for (int j = result.length - 1; j >= 0; j--)
-            System.out.println("test"+result[j]);
+            if (result[j] > 0) {
+                System.out.println(result[j]);
+            }
     }
 }
