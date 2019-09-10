466fb81c9f3de7dbf077f672a3add4b98a797181
ashustov <andrey.maxle@gmail.com>
2019-09-10 17:50:02 +0300

    added task: 5_3

--- src/PaintByStars_5_3.java
+++ src/PaintByStars_5_3.java
@@ -1,15 +1,11 @@
 public class PaintByStars_5_3 {
     public static void main(String[] args) {
         int x = 3;
-        int y = 2;
+        int y = 1;
         String star = "*";
-        String starArr[][] = new String[x][y];
         for (int i = 0; i <x; i++) {
-            starArr[0][i] = star;
-            System.out.print(starArr[0][i]+"\t");
             for (int j = 0; j < y; j++) {
-                starArr[i][j] = star;
-                System.out.print(starArr[i][j]+"\t");
+                System.out.print(star+"\t");
             }
             System.out.println();
         }
