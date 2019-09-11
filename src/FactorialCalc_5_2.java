aa989e1ed9743bdfe64ea8f1f677b47ffc89577e
ashustov <andrey.maxle@gmail.com>
2019-09-11 15:43:00 +0300

    added taskы: 5_1_1, 5_2

---
+++ src/FactorialCalc_5_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FactorialCalc_5_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input number for factorial calculation = ");
+        int factorial = 1;
+        int number = scanner.nextInt();
+        for (int i = 1; i < number; i++) {
+            factorial = factorial * (i + 1);
+        }
+        System.out.println("Factorial " + number + "! = " + factorial);
+    }
+}
---
+++ src/GuessNumberWithMoreAndLess_5_1_1.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+import static com.sun.tools.doclint.Entity.or;
+
+public class GuessNumberWithMoreAndLess_5_1_1 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int guessedNumber = random.nextInt(10);
+        System.out.println(guessedNumber);
+        Scanner sc = new Scanner(System.in);
+        int inputNumber;
+
+        do {
+            System.out.print("input number = ");
+            inputNumber = sc.nextInt();
+            if (guessedNumber != inputNumber) {
+                if (guessedNumber > inputNumber) {
+                    System.out.println("Number is bigger");
+                } else System.out.println("Number is less");
+            }
+        } while (guessedNumber == inputNumber);
+//            System.out.println("You guessed!");
+    }
+
+}
