5f0ccf3c686ed6e6af54563f69c605cc10118035
ashustov <andrey.maxle@gmail.com>
2019-09-11 12:44:24 +0300

    added task: 5_3

---
+++ src/GuessNumber_5_1.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber_5_1 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int guessedNumber = random.nextInt(10);
+        System.out.println(guessedNumber);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("input number = ");
+        int inputNumber = sc.nextInt();
+        System.out.println("inputed number = " + inputNumber);
+        if (guessedNumber == inputNumber) {
+            System.out.println("You guessed!");
+        } else System.out.println("Not guessed. Try again");
+    }
+}
